28ebba7136a25e5ac25595293da76dedb7031f77
aaleejandro01 <alejandromorilla2001@gmail.com>
2025-10-20 06:19:16 +0200

    subida video youtube

--- src/frontend/src/routes/about/+page.svelte
+++ src/frontend/src/routes/about/+page.svelte
@@ -4,6 +4,6 @@
 <h2>Video de AMG</h2>
     <ul>
         <li>
-            <a href="https://youtu.be/">Video AMG por poner</a>
+            <a href="https://youtu.be/Mp4MUI5aAO4">Video AMG por poner</a>
         </li>
     </ul>
